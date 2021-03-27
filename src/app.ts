66bae53e1d5c4fafe6b62617f53bb27fd34e0a29
nerdError <55310192+nerdError@users.noreply.github.com>
2021-03-28 00:27:19 +0300

    even better design

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,19 @@ const json: {
 }
 = require('./game/data/data.json');
 
+let div: HTMLDivElement;
 let text: HTMLLabelElement;
 
 const backConsoleLog = console.log;
 console.log = (...data: any[]) => {
     backConsoleLog(...data);
-    text.innerText += data.toString() + "\n";
+    //text.innerText += data.toString() + "\n";
+    let subDiv = document.createElement('div');
+    let label = document.createElement('label');
+    label.innerText = data.toString();
+
+    subDiv.appendChild(label);
+    div.appendChild(subDiv);
 }
 
 export let insults: Insult[] = [];
@@ -22,6 +29,8 @@ export let names: EntityName[] = []
 export function main() {
     //todo refactor?
     text = document.getElementById("text") as HTMLLabelElement;
+    div = document.getElementById("div") as HTMLDivElement;
+
     for (const key in json.insults) {
         const value = json.insults[key];
         insults.push(Object.assign({ kto: key }, value));
@@ -42,8 +51,9 @@ export function main() {
         arena.addEntity(new Entity(arena, health, damage, name));
     }
 
-    let loops = 100;
+    let loops = 1000;
     while(arena.loop < loops) {
+        if (arena.entities.length == 1) return;
         arena.doLoop();
     }
 }
