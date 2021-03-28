cdea7a81e4f2c1363db0a98dfcc64ee931a10e31
nerdError <55310192+nerdError@users.noreply.github.com>
2021-03-28 17:35:30 +0300

    version

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ export let insults: Insult[] = [];
 export let names: EntityName[] = []
 //export let items: Item[] = [];
 
-const version = "17:05, 28.03.2021"
+const version = "17:35, 28.03.2021"
 
 
 export async function main() {
